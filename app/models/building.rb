a09cd940dc9e79ef463d3b68e1e0a87178ef2bb3
Zach Banks <zachbanks@me.com>
2012-05-19 17:41:17 -0400

    Added validations to Building model.

--- app/models/building.rb
+++ app/models/building.rb
@@ -1,3 +1,11 @@
 class Building < ActiveRecord::Base
+  attr_accessible :name, :short_name, :locations
+  
   has_many :locations
+  
+  validates :name, :short_name, :presence => true
+  
+  # Makes sure you can't create copies of a building that has the same name or short name.
+  validates :short_name, :uniqueness => true
+  validates :name, :uniqueness => true
 end
