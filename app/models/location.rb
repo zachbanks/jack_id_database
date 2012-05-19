9a37a5eb48d494ee753230babce2cfd6c6863f57
Zach Banks <zachbanks@me.com>
2012-05-19 17:50:09 -0400

    Added proper room validation for Location model.

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,4 +1,9 @@
 class Location < ActiveRecord::Base
+  attr_accessible :building, :room, :jack_id, :notes
+  
   belongs_to :building
   has_many :jack_ids
+  
+  validates :building, :room, :presence => true
+  validates :room, :uniqueness => { :scope => :building_id }
 end
