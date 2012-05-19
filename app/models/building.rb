042f69b878ea5537260b1899dd5e586b43151612
Zach Banks <zachbanks@me.com>
2012-05-19 18:49:40 -0400

    Fixed uniqueness validation bug.

--- app/models/building.rb
+++ app/models/building.rb
@@ -9,11 +9,11 @@ class Building < ActiveRecord::Base
   validates :name, :short_name, :presence => true
   
   # Short name can only be 3 characters long.
-  validates :short_name, :length => { :is => 3 }
+  validates :short_name, :length => { :is => 3 , :message => " must be 3 characters long." }
   
   # Makes sure you can't create copies of a building that has the same name or short name.
-  validates :short_name, :uniqueness => true
-  validates :name, :uniqueness => true
+  validates :short_name, :uniqueness => { :case_sensitive => false } # Case does not matter.
+  validates :name, :uniqueness => { :case_sensitive => false } # Case does not matter.
   
   private 
   
@@ -22,5 +22,4 @@ class Building < ActiveRecord::Base
     self.name = name.titleize
     self.short_name = short_name.downcase
   end
-  
 end
