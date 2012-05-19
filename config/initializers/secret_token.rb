ff0ed9f23a93475ce8796cedf8aa03672685400b
Zach Banks <zachbanks@me.com>
2012-05-18 21:09:03 -0400

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+JackIdDatabase::Application.config.secret_token = '797a0d5c478b0e63ba070accd4a237bccf5779ebc91f982035de9be5ebcf380198c6e27c7514f5e35c00ae00be1b40e583908a74405dc3ee68588d7576681325'
