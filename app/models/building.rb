fe035a7e0868ec3d4943d880eedd59b159fbc3c4
Zach Banks <zachbanks@me.com>
2012-05-19 18:14:19 -0400

    Finished validations for Building model.

--- app/models/building.rb
+++ app/models/building.rb
@@ -1,11 +1,26 @@
 class Building < ActiveRecord::Base
+  before_save :format_attributes
+  
   attr_accessible :name, :short_name, :locations
   
   has_many :locations
   
+  # Validates presence of attributes.
   validates :name, :short_name, :presence => true
+  
+  # Short name can only be 3 characters long.
+  validates :short_name, :length => { :is => 3 }
   
   # Makes sure you can't create copies of a building that has the same name or short name.
   validates :short_name, :uniqueness => true
   validates :name, :uniqueness => true
+  
+  private 
+  
+  # Format attributes before saving.
+  def format_attributes
+    self.name = name.titleize
+    self.short_name = short_name.downcase
+  end
+  
 end
