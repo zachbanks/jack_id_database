0f89ebe77550973813ada0bb9330b6250dcff092
Zach Banks <zachbanks@me.com>
2012-05-19 20:38:00 -0400

    Added CRUD for Building resource.

---
+++ app/controllers/buildings_controller.rb
@@ -0,0 +1,36 @@
+class BuildingsController < ApplicationController
+  def index
+    @buildings = Building.all
+  end
+  
+  def new
+    @building = Building.new
+  end
+  
+  def create
+    @building = Building.new(params[:building])
+    if @building.save
+      redirect_to buildings_path, :notice => "#@building.name was successfully added."
+    else
+      render :new
+    end
+  end
+  
+  def edit
+    @building = Building.find(params[:id])
+  end
+  
+  def update
+    @building = Building.find(params[:id])
+    if @building.update_attributes(params[:building])
+      redirect_to buildings_path, :notice => "#@building.name was successfully updated."
+    end
+  end
+  
+  def destroy
+    @building = Building.find(params[:id])
+    msg = @building.to_s
+    @building.destroy
+    redirect_to buildings_path, :flash => { :success => msg }
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 JackIdDatabase::Application.routes.draw do
   root :to => "buildings#index"
   
+  resources :buildings
+  
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
