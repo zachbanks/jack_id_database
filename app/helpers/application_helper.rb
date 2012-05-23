17ad4b83e73199a61c18deb85e110678b3b7abb1
Zach Banks <zachbanks@me.com>
2012-05-23 02:18:19 -0400

    Setup basic layout using Twitter Bootstrap.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  # Helps format flash messages with Twitter Bootstrap CSS classes. 
+  # Is rendered in application.html.erb
+  # Method is used in shared/_flash_messages.html.erb.
+  def flash_class(level)
+    case level
+    when :notice then 'alert alert-info'
+    when :success then 'alert alert-success'
+    when :error then 'alert alert-error'
+    when :alert then 'alert alert-error'
+    end
+  end
 end
