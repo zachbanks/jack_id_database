2b146ad1cf05e2c293ac817f62eb568b67532992
Zach Banks <zachbanks@me.com>
2012-05-27 16:08:29 -0400

    Added tests for Location model.

--- spec/factories/location_factory.rb
+++ spec/factories/location_factory.rb
@@ -3,5 +3,12 @@ FactoryGirl.define do
     association :building
     sequence(:room) { |n| "320-#{n}" }
     notes 'These are some internal notes about the location.'
+    
+    factory :location_with_jack_ids do
+      ignore { jack_id_count 5 }
+      after(:create) do |location, evaluator|
+        create_list(:jack_id, evaluator.jack_id_count, :location => location)
+      end
+    end    
   end
 end
--- spec/models/location_spec.rb
+++ spec/models/location_spec.rb
@@ -1,21 +1,49 @@
 require 'spec_helper'
 
 describe Location do
-  it 'has valid factory' do
+  it 'should have valid factory' do
     create(:location)
   end
   
   describe 'validations' do
-    it 'has a building'
-    it 'has a room'
-    it 'has a unique room for each building'
+    it 'should have a building' do
+      build(:location, building: nil).should_not be_valid
+    end
+    
+    it 'should have a room' do
+      build(:location, room: nil).should_not be_valid
+    end
+    
+    it 'should have a unique room for each building' do
+      building = create(:building, name: 'Bowman')
+      room = '320-A'
+      
+      create(:location, building: building, room: room)
+      build(:location, building: building, room: room).should_not be_valid
+    end
   end
   
   describe 'associations' do
-    it 'has many jack ids'
-    it 'belongs to a building'
+    it 'should have many jack ids' do
+      build(:location_with_jack_ids).jack_ids.each { |jack_id| jack_id.should_be_an_instance_of(JackId) }
+    end
+    
+    it 'should belong to a building' do
+      build(:location).should respond_to(:building)
+    end
   end
   
-  it 'is sorted by building and then by room'
-  it 'returns a custom string representation of itself'
+  it 'should be sorted by building and then by room' do
+    bowman = create(:building, name: 'Bowman')
+    merril = create(:building, name: 'Merril')
+    loc3 = create(:location, building: merril, room: '304-X')
+    loc2 = create(:location, building: merril, room: '304-A')
+    loc1 = create(:location, building: bowman)
+    
+    Location.all.should == [loc1, loc2, loc3]
+  end
+  
+  it 'should return a custom string representation of itself' do
+    create(:location, building: create(:building, name: 'Bowman'), room: '302').to_s.should == 'Bowman - 302'
+  end
 end
