b93556c7593961360b1ab1669579ea7947d41c65
Zach Banks <zachbanks@me.com>
2012-06-22 16:56:07 -0400

    Refactored * notes indicator into helper and added it to Locations#index view as well.

--- app/helpers/locations_helper.rb
+++ app/helpers/locations_helper.rb
@@ -1,2 +1,6 @@
 module LocationsHelper
+  # Adds an * if given location has notes field that is not blank.
+  def add_associated_notes_indicator_for(location)
+    '<span class="has-notes">* </span>'.html_safe unless location.notes.blank? # html_safe allows html to be rendered in view.
+  end
 end
