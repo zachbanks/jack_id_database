a9aec2557d46baf6fe9a9a05c9228c5a6197ccf3
Zach Banks <zachbanks@me.com>
2012-08-02 21:25:30 -0400

    Removed elasticsearch.

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,8 +1,4 @@
 class Location < ActiveRecord::Base
-  include Tire::Model::Search
-  include Tire::Model::Callbacks
-  index_name BONSAI_INDEX_NAME
-  
   attr_accessible :room, :jack_ids, :notes, :building_id, :jack_ids_attributes, :building, :last_modified_by
   
   before_save :update_last_modified_at
@@ -40,27 +36,6 @@ class Location < ActiveRecord::Base
       :name => self.name_of_last_to_modify,
       :email => self.email_of_last_to_modify
     }
-  end
-  
-  # This right here is the problem method!!!
-  # This is causing new records to not show on redirect.
-  # This is causing Heroku error!
-  def self.search(params, options={})
-    tire.search(:load => true, :page => params[:page], :per_page => options[:per_page] || 15) do
-      query { string params[:query], :default_operator => 'AND' } if params[:query].present?
-    end
-  end
-  
-  def to_indexed_json
-    to_json(:methods => [:building_name, :building_short_name])
-  end
-  
-  def building_name
-    building.name
-  end
-  
-  def building_short_name
-    building.short_name
   end
 end
 
